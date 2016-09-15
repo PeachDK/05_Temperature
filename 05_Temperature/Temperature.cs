c19913c07f8056046fb6fc8deafd4ffa1e74b0d0
Pelle <Pelle@LAPTOP-T69PBCOE>
2016-09-15 10:24:52 +0200

    Add project files.

---
+++ 05_Temperature/Temperature.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _05_Temperature
+{
+    
+    public enum Unit
+    {
+        Fahrenheit,
+        Celcius
+    }
+
+    internal class Temperature
+    {
+        public double Fahrenheit, Celcius;
+        
+
+        public Temperature(Unit valg, double temp)
+        {
+            Fahrenheit = temp;
+            Celcius = temp;
+        }
+        public Temperature()
+        {
+
+        }
+
+
+        internal static double FahrenheitToCelcius(double f)
+        {
+            return (f - 32) * 5 / 9;
+
+        }
+
+        internal static double CelciusToFahrenheit(double c)
+        {
+            return (c * 9 / 5) + 32;
+        }
+
+        
+      
+        
+    }
+}
